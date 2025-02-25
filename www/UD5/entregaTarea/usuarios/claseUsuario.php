827e4165938faefe8d7178a31a3e2386affaac66
XesAM <xes.aldea.moreira@outlook.com>
2025-02-25 09:53:23 +0100

    clase_user_creada

---
+++ www/UD5/entregaTarea/usuarios/claseUsuario.php
@@ -0,0 +1,64 @@
+<?php  
+
+class user{
+    
+    private $id;
+    private $username;
+    private $name;
+    private $apellidos;
+    private $contrasena;
+    private $rol;
+
+    function __constructor($id, $username, $name, $apellidos, $contrasena, $rol){
+        $this-> id = $id;
+        $this->username = $username;
+        $this->name = $name;
+        $this-> apellidos = $apellidos;
+        $this->  contrasena = $contrasena;
+        $this-> rol = $rol;
+    }
+
+    function setId($id){
+        $this-> id = $id;
+    }
+    function getId(){
+        return $this->id;
+    }
+
+    function setUsername($username){
+        $this-> username = $username;
+    }
+    function getUsername(){
+        return $this->username;
+    }
+
+    function setName($name){
+        $this-> name = $name;
+    }
+    function getName(){
+        return $this->Name;
+    }
+
+    function setApellidos($apellidos){
+        $this-> apellidos = $apellidos;
+    }
+    function getApellidos(){
+        return $this->Apellidos;
+    }
+
+    function setContrasena($contrasena){
+        $this-> contrasena = $contrasena;
+    }
+    function getContrasena(){
+        return $this->contrasena;
+    }
+
+    function setRol($rol){
+        $this-> rol = $rol;
+    }
+    function getRol(){
+        return $this->rol;
+    }
+}
+
+?>
